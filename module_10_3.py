40fcd727ebc18c1462f8d5e8a5ca626755165b8d
Vyacheslav05 <vyacheslav.lobanov@wellpack.org>
2024-08-23 14:52:40 +0300

    Add files via upload

--- module_10_3.py
+++ module_10_3.py
@@ -8,30 +8,30 @@ class Bank:
         self.lock = threading.Lock()
 
     def deposit(self):
-        for i in range(100):
+        for _ in range(100):
             amount = random.randint(50, 500)
             self.balance += amount
-            print(f"Пополнение: {amount}. Баланс: {self.balance}")
+            print(f'Пополнение: {amount}. Баланс: {self.balance}')
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
             time.sleep(0.001)
 
     def take(self):
-        for i in range(100):
+        for _ in range(100):
             amount = random.randint(50, 500)
-            print(f"Запрос на {amount}")
+            print(f'Запрос на {amount}')
             if amount <= self.balance:
                 self.balance -= amount
-                print(f"Снятие: {amount}. Баланс: {self.balance}")
+                print(f'Снятие: {amount}. Баланс: {self.balance}')
             else:
-                print("Запрос отклонён, недостаточно средств")
+                print('Запрос отклонён, недостаточно средств')
                 self.lock.acquire()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     bk = Bank()
 
-    th1 = threading.Thread(target=bk.deposit)
-    th2 = threading.Thread(target=bk.take)
+    th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+    th2 = threading.Thread(target=Bank.take, args=(bk,))
 
     th1.start()
     th2.start()
@@ -39,4 +39,4 @@ if __name__ == "__main__":
     th1.join()
     th2.join()
 
-    print(f"Итоговый баланс: {bk.balance}")
+    print(f'Итоговый баланс: {bk.balance}')
