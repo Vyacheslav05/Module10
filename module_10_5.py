98503411854fdd3995243c2572df52684587aa66
Vyacheslav05 <vyacheslav.lobanov@wellpack.org>
2024-08-31 12:44:11 +0300

    Add files via upload

--- module_10_5.py
+++ module_10_5.py
@@ -2,13 +2,11 @@ import time
 import multiprocessing
 
 def read_info(name):
-    all_data = []
     with open(name, 'r') as file:
         while True:
             line = file.readline()
             if not line:
                 break
-            all_data.append(line)
 
 filenames = [f'./file {number}.txt' for number in range(1, 5)]
 
