1625c36f26b3b3ad4c3f2d13a87433505cd18174
Vyacheslav05 <vyacheslav.lobanov@wellpack.org>
2024-08-31 12:30:04 +0300

    Add files via upload

---
+++ module_10_5.py
@@ -0,0 +1,28 @@
+import time
+from multiprocessing import Pool
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            all_data.append(line)
+
+if __name__ == '__main__':
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+    # Линейный вызов
+    start_time = time.time()
+    for filename in filenames:
+        read_info(filename)
+    linear_time = time.time() - start_time
+    print(f'Линейный вызов: {linear_time}')
+
+    # Многопроцессный вызов
+    start_time = time.time()
+    with Pool() as pool:
+        pool.map(read_info, filenames)
+    multiprocess_time = time.time() - start_time
+    print(f'Многопроцессный вызов: {multiprocess_time}')
